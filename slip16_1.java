e2368895c52ce523b2680528bd8a9c3948ca86a9
gayatri-kulkarni24 <gaytrikulkarni616@gmail.com>
2026-01-08 19:09:56 +0530

    Create slip16_1.java

---
+++ slip16_1.java
@@ -0,0 +1,19 @@
+package javaSlips2025;
+
+import java.util.TreeSet;
+
+public class slip16_1 {
+
+	public static void main(String[] args) {
+		TreeSet<String> ts=new TreeSet<String>();
+		ts.add("red");
+		ts.add("blue");
+		ts.add("azure");
+		ts.add("green");
+		ts.add("yellow");
+		System.out.println(ts);
+		
+
+	}
+
+}
